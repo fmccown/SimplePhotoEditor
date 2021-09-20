19d7ab9d7b6510b454ddffad4b50b215ed2a697c
Frank McCown <fmccown@harding.edu>
2021-09-20 10:54:23 -0500

    Add project

---
+++ PhotoEditorSimple/PhotoEditorSimple/MainForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace PhotoEditorSimple
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.photoListComboBox = new System.Windows.Forms.ComboBox();
+            this.photoDirLabel = new System.Windows.Forms.Label();
+            this.transformButton = new System.Windows.Forms.Button();
+            this.photoPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.photoPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // photoListComboBox
+            // 
+            this.photoListComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.photoListComboBox.FormattingEnabled = true;
+            this.photoListComboBox.Location = new System.Drawing.Point(30, 35);
+            this.photoListComboBox.Name = "photoListComboBox";
+            this.photoListComboBox.Size = new System.Drawing.Size(210, 21);
+            this.photoListComboBox.TabIndex = 0;
+            this.photoListComboBox.SelectedIndexChanged += new System.EventHandler(this.photoListComboBox_SelectedIndexChanged);
+            // 
+            // photoDirLabel
+            // 
+            this.photoDirLabel.AutoSize = true;
+            this.photoDirLabel.Location = new System.Drawing.Point(27, 19);
+            this.photoDirLabel.Name = "photoDirLabel";
+            this.photoDirLabel.Size = new System.Drawing.Size(48, 13);
+            this.photoDirLabel.TabIndex = 1;
+            this.photoDirLabel.Text = "photo dir";
+            // 
+            // transformButton
+            // 
+            this.transformButton.Location = new System.Drawing.Point(270, 33);
+            this.transformButton.Name = "transformButton";
+            this.transformButton.Size = new System.Drawing.Size(75, 23);
+            this.transformButton.TabIndex = 2;
+            this.transformButton.Text = "Transform";
+            this.transformButton.UseVisualStyleBackColor = true;
+            this.transformButton.Click += new System.EventHandler(this.transformButton_Click);
+            // 
+            // photoPictureBox
+            // 
+            this.photoPictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.photoPictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.photoPictureBox.Location = new System.Drawing.Point(30, 79);
+            this.photoPictureBox.Name = "photoPictureBox";
+            this.photoPictureBox.Size = new System.Drawing.Size(315, 330);
+            this.photoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.photoPictureBox.TabIndex = 3;
+            this.photoPictureBox.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 450);
+            this.Controls.Add(this.photoPictureBox);
+            this.Controls.Add(this.transformButton);
+            this.Controls.Add(this.photoDirLabel);
+            this.Controls.Add(this.photoListComboBox);
+            this.Name = "MainForm";
+            this.Text = "Simple Photo Editor";
+            ((System.ComponentModel.ISupportInitialize)(this.photoPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox photoListComboBox;
+        private System.Windows.Forms.Label photoDirLabel;
+        private System.Windows.Forms.Button transformButton;
+        private System.Windows.Forms.PictureBox photoPictureBox;
+    }
+}
+
---
+++ PhotoEditorSimple/PhotoEditorSimple/MainForm.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PhotoEditorSimple
+{
+    public partial class MainForm : Form
+    {
+        private string photoRootDirectory;
+        private List<FileInfo> photoFiles;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            photoRootDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            photoDirLabel.Text = photoRootDirectory;
+
+            PopulateImageList();
+        }
+
+        private void PopulateImageList()
+        {
+            photoFiles = new List<FileInfo>();
+
+            DirectoryInfo homeDir = new DirectoryInfo(photoRootDirectory);
+            foreach (FileInfo file in homeDir.GetFiles("*.jpg"))
+            {
+                photoFiles.Add(file);
+            }
+
+            photoListComboBox.Items.AddRange(photoFiles.ToArray());
+        }
+
+        private void photoListComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var selectedFile = (FileInfo) photoListComboBox.SelectedItem;
+            photoPictureBox.Image = LoadImage(selectedFile.FullName);
+        }
+
+        private Image LoadImage(string filename)
+        {
+            // Use this method to load images so the image files do not remain locked
+            byte[] bytes = File.ReadAllBytes(filename);
+            MemoryStream ms = new MemoryStream(bytes);
+            return Image.FromStream(ms);
+        }
+
+        private void transformButton_Click(object sender, EventArgs e)
+        {
+            var transformedBitmap = new Bitmap(photoPictureBox.Image);
+
+            // This could take a long time... should be done in a thread
+            InvertColors(transformedBitmap);
+
+            photoPictureBox.Image = transformedBitmap;
+        }
+
+        // Inverts each pixel
+        private void InvertColors(Bitmap transformedBitmap)
+        {
+            for (int y = 0; y < transformedBitmap.Height; y++)
+            {
+                for (int x = 0; x < transformedBitmap.Width; x++)
+                {
+                    var color = transformedBitmap.GetPixel(x, y);
+                    int newRed = Math.Abs(color.R - 255);
+                    int newGreen = Math.Abs(color.G - 255);
+                    int newBlue = Math.Abs(color.B - 255);
+                    Color newColor = Color.FromArgb(newRed, newGreen, newBlue);
+                    transformedBitmap.SetPixel(x, y, newColor);
+                }
+            }
+        }
+
+        // Tints image with chosen color
+        private void AlterColors(Bitmap transformedBitmap, Color chosenColor)
+        {
+            for (int y = 0; y < transformedBitmap.Height; y++)
+            {
+                for (int x = 0; x < transformedBitmap.Width; x++)
+                {
+                    var color = transformedBitmap.GetPixel(x, y);
+                    int ave = (color.R + color.G + color.B) / 3;
+                    double percent = ave / 255.0;
+                    int newRed = Convert.ToInt32(chosenColor.R * percent);
+                    int newGreen = Convert.ToInt32(chosenColor.G * percent);
+                    int newBlue = Convert.ToInt32(chosenColor.B * percent);
+                    var newColor = Color.FromArgb(newRed, newGreen, newBlue);
+                    transformedBitmap.SetPixel(x, y, newColor);
+                }
+            }
+        }
+
+        // brightness is a value between 0 – 100. Values < 50 makes the image darker, > 50 makes lighter
+        private void ChangeBrightness(Bitmap transformedBitmap, int brightness)
+        {
+            // Calculate amount to change RGB
+            int amount = Convert.ToInt32(2 * (50 - brightness) * 0.01 * 255);
+            for (int y = 0; y < transformedBitmap.Height; y++)
+            {
+                for (int x = 0; x < transformedBitmap.Width; x++)
+                {
+                    var color = transformedBitmap.GetPixel(x, y);
+                    int newRed = Math.Max(0, Math.Min(color.R - amount, 255));
+                    int newGreen = Math.Max(0, Math.Min(color.G - amount, 255));
+                    int newBlue = Math.Max(0, Math.Min(color.B - amount, 255));
+                    var newColor = Color.FromArgb(newRed, newGreen, newBlue);
+                    transformedBitmap.SetPixel(x, y, newColor);
+                }
+            }
+        }
+    }
+}
